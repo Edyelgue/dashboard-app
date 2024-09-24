81fde56080926cfa83167e9e05914b515827d644
Edyelgue Antonio dos Santos Carneiro <edyelgue.carneiro@desktop.tec.br>
2024-09-24 17:52:43 -0300

    criado template do grafico

--- resources/views/time-assigned.blade.php
+++ resources/views/time-assigned.blade.php
@@ -1,11 +1,11 @@
 @include('layouts.header')
 <section class="text-gray-600 body-font">
   <div class="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
-    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+    <canvas id="myChart"></canvas>
   </div>
 </section>
 <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
+  <div class="container px-1 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
       <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Incidentes</h1>
     </div>
@@ -31,4 +31,40 @@
     </div>
   </div>
 </section>
+
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    // Criação dos dados para o gráfico
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'myChart',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgb(255, 99, 132)'],
+        borderWidth: 1
+      }]
+    };
+
+    // Configuração do gráfico
+    const config = {
+      type: 'line', // ou 'bar', 'pie', etc.
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    };
+
+    // Renderização do gráfico no canvas
+    const myChart = new Chart(
+      document.getElementById('myChart'),
+      config
+    );
+  });
+</script>
 @include('layouts.footer')
