052b057a668c39d940f0726d3fcbbc7bed05f0b1
Edyelgue Carneiro <edyelgue12@gmail.com>
2024-09-24 22:18:55 -0300

    atualização changestatusDTO

--- app/Http/Controllers/ChangeStatusController.php
+++ app/Http/Controllers/ChangeStatusController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\ChangeStatusDTO;
+use Carbon\Carbon;
 
 class ChangeStatusController extends Controller
 {
     public function index()
     {
-        // Chamando o método listar() e passando os dados para a view
+        // Chamando o método listar()
         $changes = ChangeStatusDTO::listar();
+
+        // Formatando as datas
+        foreach ($changes as $change) {
+            $change->createdate = Carbon::parse($change->createdate)->format('d/m/Y H:i:s');
+            $change->modifieddate = Carbon::parse($change->modifieddate)->format('d/m/Y H:i:s');
+        }
+
+        // Passando os dados formatados para a view
         return view('time-assigned', ['changes' => $changes]);
     }
 }
+
--- app/Models/ChangeStatusDTO.php
+++ app/Models/ChangeStatusDTO.php
@@ -16,11 +16,30 @@ class ChangeStatusDTO
                 'modifieddate',
                 'worklogsubmitter',
                 'incidentid',
-                DB::raw('(JULIANDAY(modifieddate) - JULIANDAY(createdate)) AS DiferencaEmHoras')
+                DB::raw('((JULIANDAY(modifieddate) - JULIANDAY(createdate)) * 24) AS DiferencaEmHoras')
             )
-            ->where('createdate', '>=', now()->subDays(7)->format('Y-m-d H:i:s'))
+            ->where('createdate', '>=', now()->subDays(1)->format('Y-m-d H:i:s'))
             ->where('notes', 'like', '%Assigned To: %')
             ->where('notes', 'NOT LIKE', '%Assigned To: helix.user%')
+            ->where('incidentsummary', 'NOT LIKE', '%GMUD%')
+            ->whereIn('worklogsubmitter', [
+                'edgard.araujo',
+                'marcos.jesus',
+                'samuel.fagundes',
+                'samuel.souza',
+                'evandro.pereira',
+                'camilly.psilva',
+                'gabriel.martins',
+                'mateus.tofani',
+                'murilo.medeiros',
+                'otavio.souza',
+                'lucas.angelo',
+                'eduardo.rezende',
+                'jepherson.lins',
+                'pedro.santos',
+                'rafael.olima',
+                'vinicius.mareti'
+            ])
             ->orderBy('modifieddate', 'DESC')
             ->get();
     }
